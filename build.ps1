298e183c9459df41955add1fa58bb1d65c3db44f
Alena Hrynkevich <alena@appveyor.com>
2019-08-14 12:05:27 -0700

    Added LastExitCode check

--- build.ps1
+++ build.ps1
@@ -1,5 +1,10 @@
-echo appveyor version
-echo git --version
+Write-Host "appveyor version: "  
+appveyor version
+Write-Host "git version: "  
+git --version
 #- git config --global core.longpaths true
-echo md C:\Users\appveyor\AppData\Local\Temp\1\npm-2900-8b1a0fe2\git-cache-397d6243\2430cdb3ef7fc9e1a93d9ed0f7cb8321679d7963
-echo git -c core.longpaths=true clone https://github.com/FeodorFitsner/git-long-paths.git C:\Users\appveyor\AppData\Local\Temp\1\npm-2900-8b1a0fe2\git-cache-397d6243\2430cdb3ef7fc9e1a93d9ed0f7cb8321679d7963
+Write-Host "md C:\Users\appveyor\AppData\Local\Temp\1\npm-2900-8b1a0fe2\git-cache-397d6243\2430cdb3ef7fc9e1a93d9ed0f7cb8321679d7963"  
+md C:\Users\appveyor\AppData\Local\Temp\1\npm-2900-8b1a0fe2\git-cache-397d6243\2430cdb3ef7fc9e1a93d9ed0f7cb8321679d7963
+Write-Host "git -c:" 
+git -c core.longpaths=true clone https://github.com/FeodorFitsner/git-long-paths.git C:\Users\appveyor\AppData\Local\Temp\1\npm-2900-8b1a0fe2\git-cache-397d6243\2430cdb3ef7fc9e1a93d9ed0f7cb8321679d7963
+if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
